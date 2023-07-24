566bb95c1537b08b8cf39d5460ac44d7e34a1869
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-07-23 23:46:55 -0300

    update example 07

--- examples/07-stack_ops.s
+++ examples/07-stack_ops.s
@@ -12,4 +12,34 @@ main:
 	OUT
 	OUT
 	OUT
+	
+	PSH 999			; push 3rd argument
+	PSH 888			; push 2nd argument
+	PSH 777			; push 1st argument
+	PSH ret1		; push return address
+	PSHSP			; push stack pointer
+	BRA func1		; call func1
+ret1:
+	PSH 16			; pop 4 arguments (4 * 4 bytes)
+	ADD
+	POPSP			; update stack pointer
 	HLT
+	
+func1:
+	DUP
+	PSH 8
+	ADD
+	LDW			; load 1st argument
+	OUT
+	DUP
+	PSH 12
+	ADD
+	LDW			; load 2nd argument
+	OUT
+	DUP
+	PSH 16
+	ADD
+	LDW			; load 3rd argument
+	OUT
+	OVER			; get return address
+	JMP			; return
