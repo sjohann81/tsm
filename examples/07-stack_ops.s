135e9fb8af49e0e2c82b07aee6cb2eb1e1e3f8bf
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-07-27 00:40:01 -0300

    update example 07

--- examples/07-stack_ops.s
+++ examples/07-stack_ops.s
@@ -12,34 +12,43 @@ main:
 	OUT
 	OUT
 	OUT
-	
+
 	PSH 999			; push 3rd argument
 	PSH 888			; push 2nd argument
 	PSH 777			; push 1st argument
-	PSH ret1		; push return address
+	PSHFP			; push frame pointer
 	PSHSP			; push stack pointer
+	PSH ret1		; push return address
 	BRA func1		; call func1
 ret1:
-	PSH 16			; pop 4 arguments (4 * 4 bytes)
-	ADD
-	POPSP			; update stack pointer
+	POPSP
+	POPFP
+	POP
+	POP
+	POP
 	HLT
-	
+
+; arguments in stack: RA, SP, FP, 777, 888, 999
 func1:
-	DUP
-	PSH 8
-	ADD
-	LDW			; load 1st argument
-	OUT
-	DUP
-	PSH 12
-	ADD
-	LDW			; load 2nd argument
-	OUT
-	DUP
+	PSHSP
+	POPFP			; set frame pointer
+	
+	PSHFP
 	PSH 16
 	ADD
-	LDW			; load 3rd argument
+	LDW			; load 1st argument (SP + 16)
 	OUT
-	OVER			; get return address
-	JMP			; return
+	
+	PSHFP
+	PSH 20
+	ADD
+	LDW			; load 2nd argument (SP + 20)
+	OUT
+
+	PSHFP
+	PSH 24
+	ADD
+	LDW			; load 3rd argument (SP + 24)
+	OUT
+
+	JMP			; return (RA in stack)
